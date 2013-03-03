cf85c35e4280e5d1ea91863e24d509b35d1178b0
Rafael Almeida Erthal Hermano <rafaerthal@gmail.com>
2013-03-02 21:38:50 -0300

    Update namespace.js

--- namespace.js
+++ namespace.js
@@ -1,7 +1,8 @@
 var Namespace = new Class(function (files, callback) {
     var ajax = new Ajax(),
         sources = [],
-        i;
+        i,
+        count = 0;
 
     var Builder = new Class(function (fn) {
         this.data = null;
@@ -25,84 +26,32 @@ var Namespace = new Class(function (files, callback) {
     });
 
     for (i in files) {
-        //Pega o código de cada arquivo
-        ajax.get(files[i], function (data) {
+        count++;
+    }
+
+    var save = function (name) {
+        ajax.get(files[name], function (data) {
             var i,
                 space = {};
             //Grava o código e o nome do arquivo
             sources.push({
-                name   : i
-                source : new Function(data);
+                name   : name,
+                source : new Function(data)
             });
             //Verifica se todos os arquivos ja foram entregues
-            if (sources.length === files.length) {
+            if (sources.length === count) {
                 for (i in sources) {
                     //Coloca os objetos no resultado
                     space[sources[i].name] = (new Builder(sources[i].source)).data;
                 }
                 //Responde o namespace
-                callback(space);
+                callback.apply(space);
             }
         });
     }
-});
 
-
-
-/*--------------- home.js ---------------*/
-
-new Namespace({
-    models      : '/models/models.js'
-}, function () {
-    var dog = new this.models.Dog('odie');
-    var cat = new this.models.Cat('garfield');
-    
-    dog.bark();
-    cat.meow();
-});
-
-/*-------------- models.js --------------*/
-
-var exports = this.exports;
-
-new Namespace({
-    Animal : '/models/animal.js',
-    Dog    : '/models/dog.js',
-    Cat    : '/models/cat.js',
-}, function () {
-    exports(this);
-});
-
-/*-------------- Animal.js --------------*/
-
-this.exports(new Class(function (name) {
-    this.name = name;
-    
-    this.say = function (message) {
-        console.log(this.name + ' says: ' + message);
+    for (i in files) {
+        //Pega o código de cada arquivo
+        save(i);
     }
-}));
-
-/*-------------- dog.js --------------*/
-
-var Animal = this.use('Animal');
-
-this.exports(new Class(function () {
-    this.inherits(Animal);
-    
-    this.bark = function () {
-        this.say('Au Au!');
-    }
-}));
-
-/*-------------- cat.js --------------*/
-
-var Animal = this.use('Animal');
-
-this.exports(new Class(function () {
-    this.inherits(Animal);
-    
-    this.meow = function () {
-        this.say('Meow!');
-    }
-}));
+});
